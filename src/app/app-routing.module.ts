1037a947976a43ebbfc3c5edcfe9d7856d0fef81
maschad <chad.nehemiah94@gmail.com>
2017-07-10 17:42:52 -0500

    added two more components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {LandingComponent} from "./landing/landing.component";
+import {SignupComponent} from "./signup/signup.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    {path: '', component: LandingComponent},
+    {path: '/login', component: LoginComponent},
+    {path:'/signup', component: SignupComponent}
 ];
 
 @NgModule({
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,24 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { AppComponent } from './app.component';
+import {Observable} from "rxjs";
+import {Router, ActivatedRoute} from "@angular/router";
 
 describe('AppComponent', () => {
+
+let app: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
+
+class RouterStub {
+    navigate(params) {
+
+    }
+}
+
+class ActivatedRouteStub {
+    params: Observable<any> = Observable.empty();
+}
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -12,25 +27,34 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+        providers: [
+            {provide: Router, useClass: RouterStub },
+            {provide: ActivatedRoute, useClass: ActivatedRouteStub }
+        ]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it(`should have as title 'Cloudcast'`, async(() => {
+    expect(app.title).toEqual('Cloudcast');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Cloudcast!');
+  }));
+
+  it('should be the default route', async(() => {
+      let router = TestBed.get(Router);
+
   }));
 });
