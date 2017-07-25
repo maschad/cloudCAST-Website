ebe02caddedf56c8f450fc88304b5e91b07493c7
maschad <chad.nehemiah94@gmail.com>
2017-07-25 12:50:11 -0500

    updated express server

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ const compression = require('compression');
 // Gzip
 app.use(compression());
 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 // If an incoming request uses
 // a protocol other than HTTPS,
@@ -44,3 +39,9 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
